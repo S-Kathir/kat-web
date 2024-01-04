3f4e2392587aaed3c6a306ad59c1b1c80c5b12eb
Kathir <116286219+S-Kathir@users.noreply.github.com>
2024-01-04 17:11:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,14 @@
 const quotes = [
   "Courage is not the absence of fear, but the triumph over it. - Nelson Mandela",
   "Hope is being able to see that there is light despite all of the darkness. - Desmond Tutu",
-  // Add more quotes
+  “You will never be happy if you continue to search for what happiness consists of. You will never live if you are looking for the meaning of life. ― Albert Camus",
+  
+“For every minute you are angry you lose sixty seconds of happiness. ― Ralph Waldo Emerson",
+  
+“Count your age by friends, not years. Count your life by smiles, not tears.― John Lennon",
+
+“They say a person needs just three things to be truly happy in this world: someone to love, something to do, and something to hope for. ― Tom Bodett"
+“Happiness is a warm puppy. ― Charles M. Schulz"
 ];
 
 // Select a random quote
