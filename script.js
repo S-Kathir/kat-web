0bd86f12a24cf044547c2fdece87fab907864062
Kathir <116286219+S-Kathir@users.noreply.github.com>
2024-01-04 17:05:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 // Sample quotes
+// Sample quotes
 const quotes = [
   "Courage is not the absence of fear, but the triumph over it. - Nelson Mandela",
   "Hope is being able to see that there is light despite all of the darkness. - Desmond Tutu",
   // Add more quotes
 ];
 
-// Display quotes
+// Select a random quote
+const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+// Display quote
 const quotesSection = document.getElementById("quotes");
-quotes.forEach(quote => {
-  const quoteElement = document.createElement("div");
-  quoteElement.innerText = quote;
-  quotesSection.appendChild(quoteElement);
-});
+const quoteElement = document.createElement("div");
+quoteElement.innerText = randomQuote;
+quotesSection.appendChild(quoteElement);
 
 // Sample songs
 const songs = [
